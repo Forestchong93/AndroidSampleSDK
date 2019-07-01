da60d8d71813b7f1cf9530f38fd9193342fe51f3
Forest Chong <kokhengc.forest@gmail.com>
2019-07-01 12:50:35 +0800

    - update

---
+++ Android-Sample-SDK/samplelibrary/src/main/java/com/bubblespot/samplelibrary/core/LibCore.java
@@ -0,0 +1,9 @@
+package com.bubblespot.samplelibrary.core;
+
+import android.util.Log;
+
+public class LibCore {
+    public static void saidHello(){
+        Log.i("LibCore", "say Hello");
+    }
+}
